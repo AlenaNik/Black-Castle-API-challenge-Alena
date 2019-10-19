501f6152316d4c49ad2307caaec65213d734283c
AlenaNik <alena.nik.digital@gmail.com>
2019-10-19 12:34:04 +0200

    uploaded all of the files given in the challengeto a new repo

---
+++ src/api.js
@@ -0,0 +1,23 @@
+const { Router } = require("express");
+const api = Router();
+
+// This will be your data source
+const players = [
+  { id: 1, name: "Jon Snow", age: 23, health: 100, bag: [1] },
+  { id: 2, name: "Littlefinger", age: 35, health: 100, bag: [2] },
+  { id: 3, name: "Daenerys Targaryen", age: 20, health: 100, bag: [3] },
+  { id: 4, name: "Samwell Tarly", age: 18, health: 100, bag: [4] }
+];
+const objects = [
+  { id: 1, name: "spoon", value: -1 },
+  { id: 2, name: "knife", value: -10 },
+  { id: 3, name: "sword", value: -20 },
+  { id: 4, name: "potion", value: +20 }
+];
+
+// EXAMPLE ENDPOINT: LIST ALL OBJECTS
+api.get("/objects", function(req, res) {
+  res.json(objects);
+});
+
+module.exports = api;
